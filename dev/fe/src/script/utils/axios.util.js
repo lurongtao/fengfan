dbaf22876ebb102db167f18e0d0a1229745d4fe6
lurongtao <lurongtao@126.com>
2017-05-28 11:38:44 +0800

    接口调整

--- dev/fe/src/script/utils/axios.util.js
+++ dev/fe/src/script/utils/axios.util.js
@@ -14,27 +14,14 @@ const Axios = {
   },
 
   post: (uri, params, cb)=>{
-    axios.get(uri, {
-      params: params
+    axios.post(uri, {
+      data: params
     })
     .then(function (response) {
       cb(response)
     })
     .catch(function (error) {
       console.log(error);
-    })
-  },
-  lgypost: (param)=>{//lgy加入，用于登录
-    axios({
-      url: param.url,
-      method: param.method,
-      data: param.data,
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8'
-      }
-    })
-    .then(function (res) {
-      param.callback(res)
     })
   }
 }
